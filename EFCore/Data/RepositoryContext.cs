64fb3ac0f761b7b1810dc73f2b103841401d2808
Ricardo Orozco <mrmkm@live.com>
2022-04-21 12:58:28 -0500

    Initial Commit

---
+++ EFCore/Data/RepositoryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Data
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Blogging;Integrated Security=True";
+
+            options.UseSqlServer(connection);
+        }
+    }
+}
---
+++ EFCore/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogID { get; set; } 
+        public Blog blog { get; set; } 
+
+    }
+}
